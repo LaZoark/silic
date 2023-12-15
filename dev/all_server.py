16ad642195a8296863f4de68c117d6b7052afdc8
LaZoark <emersonchen086@icloud.com>
2023-12-16 03:33:13 +0800

    init all_server

---
+++ dev/all_server.py
@@ -0,0 +1,156 @@
+# V4 this sample by restful api trigger and store the result to pg, modify for mic-710aix
+import pandas as pd, silic
+import os
+from minio import Minio
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from datetime import datetime
+import shutil
+
+# from IPython.display import Image
+# from google.colab import files
+
+app = Flask(__name__)
+
+RESULT_BASE_PATH: str = os.path.join(os.getcwd(), "result_silic")
+PG_CSV_FILE_PATH: str = RESULT_BASE_PATH + "/label/labels.csv"
+# PG_DATABASE_URL: str = "postgresql://188ae346-2065-47af-8df3-a14008cb7f29:ng7t27G4ciAwShQHNN72n0Lce@60.250.255.162:5432/silic-result"
+PG_DATABASE_URL: str = "postgresql://silic:silic@localhost:5432/silic"
+PG_SCHEMA_NAME: str = "silic"
+PG_TABLE_NAME: str = "silic_labels_table"
+PG_TARGETCLASSES_M: str = "1, 11, 12, 28, 29, 35, 40, 41, 72, 73, 76, 77, 78, 85, 86, 103, 105, 110, 126, 187, 199, 202, 235, 290, 307, 308, 313, 315, 316, 320, 330, 332, 333, 340, 341, 342, 344, 383, 419, 420, 459, 463, 465, 468, 553, 554, 555, 563, 565, 566"
+
+SILIC_MODEL: str = "./exp29"
+SILIC_STEP: int = 1000
+SILIC_CONF_THRES: float = 0.5
+
+MINIO_ENDPOINT: str = "localhost:9010"
+ACCESS_KEY: str = "admin"
+SECRET_KEY: str = "987654321"
+BUCKET_NAME: str = "silic-bucket"
+
+
+
+
+@app.route("/silic", methods=["POST"])
+def slic_browser():
+  home_base_path = os.path.join(
+    os.getcwd(),
+    datetime.now().strftime("%Y-%m-%d")
+    )
+  try:
+    # 获取传入的文件名
+    file_name = request.json["file_name"]
+    # file_name = request.form.get('file_name')
+    download_path = os.path.join(home_base_path, file_name)
+    print(f"file_path: {file_name}")
+    print(f"download_path: {download_path}")
+
+    # Use local record file
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    folder = os.getcwd()
+    localfile_path = os.path.join(folder, current_date, file_name)
+    print(f"localfile_path: {localfile_path}")
+    shutil.copy(localfile_path, download_path)
+
+    # download the file from S3
+    minio_client = Minio(
+      endpoint=MINIO_ENDPOINT, 
+      access_key=ACCESS_KEY, 
+      secret_key=SECRET_KEY, 
+      secure=False
+    )
+    minio_client.fget_object(BUCKET_NAME, file_name, download_path)
+
+    # classed by silic
+    os.makedirs(download_path, exist_ok=True)     # 确保下载目录存在
+
+    silic.browser(
+      source=download_path,
+      model=SILIC_MODEL,
+      step=SILIC_STEP,
+      targetclasses=PG_TARGETCLASSES_M,
+      conf_thres=SILIC_CONF_THRES,
+      zip=False,  # zip=false表示不產生zip檔
+    )  
+
+    # 调用函数删除特定类型的文件（例如删除路径'./samples3/'下的file）
+    delete_specific_files(home_base_path, file_name)
+    shutil.rmtree(os.path.join(RESULT_BASE_PATH, "audio"))
+    shutil.rmtree(os.path.join(RESULT_BASE_PATH, "linear"))
+    shutil.rmtree(os.path.join(RESULT_BASE_PATH, "rainbow"))
+
+    upload_data_to_postgresql(
+      csv_file_path=PG_CSV_FILE_PATH,
+      database_url=PG_DATABASE_URL,
+      schema_name=PG_SCHEMA_NAME,
+      table_name=PG_TABLE_NAME,
+      if_exists="replace"
+    )
+    return (
+      jsonify({"message": "Successfully processed the file and restore to pg archive."}),
+      200
+      )
+
+  except Exception as e:
+    return jsonify({"error": str(e)}), 500
+
+
+# delete the download file
+def delete_specific_files(directory: str, file_extension: str):
+  # 获取路径下的所有文件和目录列表
+  file_list = os.listdir(directory)
+  # 删除特定类型的文件
+  for file in file_list:
+    if file.endswith(file_extension):
+      file_path = os.path.join(directory, file)
+      try:
+        os.remove(file_path)
+        print("remove the download file success...")
+      except Exception as e:
+        print(f"Error. Please make sure {file_path} is closed. [log: {e}]")
+
+
+# Store the result to pg
+def upload_data_to_postgresql(
+  csv_file_path: str,
+  database_url: str,
+  schema_name: str,
+  table_name: str,
+  if_exists: str="replace"
+):
+  try:
+    # 读取 labels.csv 文件
+    df = pd.read_csv(csv_file_path)
+    # 连接到 PostgreSQL 数据库
+    engine = create_engine(database_url)
+    # 创建一个新的Schema对象
+    schema = MetaData(schema=schema_name)
+    # 将数据上传至 PostgreSQL 数据库, 'replace'刪除取代現有資料, 'append'加入新的資料
+    df.to_sql(
+      name=table_name, 
+      con=engine, 
+      schema=schema_name, 
+      if_exists="append", 
+      # if_exists=if_exists, 
+      index=False
+    )
+    print("Store to PostgreSQL success!")
+  except Exception as e:
+    print(f"Error uploading data to PostgreSQL: {str(e)}")
+
+
+if __name__ == "__main__":
+  # Connect to S3 parament
+  # minio_endpoint = "blobstore.education.wise-paas.com:8888"
+  # access_key = "836f6e5b71294a50989599f54a63f628"
+  # secret_key = "xqDXwM57kYuA01ptpToWbtuj5SzSKAFzc"
+  # bucket_name = "ntutony-demo"
+
+  # file_name = 'recording_20230804_170020.mp3'    # get file name
+  # file_path = file_name                          # get file path
+  # download_path = "./samples3/"+file_name
+  # folder = "/mnt/usb/audio_record_data/"  # 於/mnt/usb/目錄下
+
+  # start the restful
+  app.run(host="0.0.0.0", port=5000)
