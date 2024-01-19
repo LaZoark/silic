6c92af09548540f809adb4e346f70c5dc61f7925
LaZoark <emersonchen086@icloud.com>
2024-01-19 16:59:33 +0800

    optimize the testing method and crontab

--- dev/test_pyaudio.py
+++ dev/test_pyaudio.py
@@ -1,9 +1,21 @@
 import pyaudio
+print(f"[DEBUG warn] --------------------------- 0 -------------------------------")
+
+### Suppressing pyaudio error logs
+from ctypes import *
+# From alsa-lib Git 3fd4ab9be0db7c7430ebd258f2717a976381715d
+# Define our error handler type
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+def py_error_handler(filename, line, function, err, fmt):
+  pass
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+asound = cdll.LoadLibrary('libasound.so')
+asound.snd_lib_error_set_handler(c_error_handler) # Set error handler
 
 pyaudio_instance = pyaudio.PyAudio()
 
 
-print('\navailable devices:')
+print('\nAvailable devices:')
 
 for i in range(pyaudio_instance.get_device_count()):
   dev = pyaudio_instance.get_device_info_by_index(i)
@@ -11,15 +23,24 @@ for i in range(pyaudio_instance.get_device_count()):
   print(f"[{i=}], {name}, \t\tmaxInputChannels={dev['maxInputChannels']}, maxOutputChannels={dev['maxOutputChannels']}")
 
 print('\ndefault input & output device:')
-print(f"default_input_device_info  = {pyaudio_instance.get_default_input_device_info()}")
-print(f"default_output_device_info = {pyaudio_instance.get_default_output_device_info()}")
-
+try:
+  print(f"[DEBUG warn] --------------------------- 1 -------------------------------")
+  print(f"default_input_device_info  = {pyaudio_instance.get_default_input_device_info()}")
+  print(f"[DEBUG warn] --------------------------- 2 -------------------------------")
+  print(f"default_output_device_info = {pyaudio_instance.get_default_output_device_info()}")
+  print(f"[DEBUG warn] --------------------------- 3 -------------------------------")
+except Exception as e:
+  print(f"[DEBUG warn] --------------------------- 4 -------------------------------")
+  print(f"{e}")
+  print(f"[DEBUG warn] --------------------------- 5 -------------------------------")
 
 stream = pyaudio_instance.open(
     format=pyaudio.paInt16, 
     channels=1, 
     rate=44100, 
     input=True, 
-    # input_device_index=3,
+    # input_device_index=0,
     frames_per_buffer=1024
   )
+
+print("Success!")
--- mount_usb.sh
+++ mount_usb.sh
@@ -1,4 +1,4 @@
-echo -e '#!/bin/sh\nsudo mkdir -p /media/silic\nsudo umount /dev/sda1\nsudo mount /dev/sda1 -t vfat -o rw,nosuid,nodev,sync,noatime,quiet,shortname=mixed,uid=1000,gid=1000,umask=077,iocharset=utf8 /media/silic\necho 'mount to /media/silic'\n' | sudo tee /etc/init.d/mount-usb > /dev/null
+echo -e '#!/bin/sh\nsudo mkdir -p /media/silic\nsudo umount /dev/sda1 || echo "Skipping..."\nsudo mount /dev/sda1 -t vfat -o rw,nosuid,nodev,sync,noatime,quiet,shortname=mixed,uid=1000,gid=1000,umask=077,iocharset=utf8 /media/silic || echo "error: " \necho "mount to /media/silic"\n' | sudo tee /etc/init.d/mount-usb > /dev/null
 sudo chmod +x /etc/init.d/mount-usb
 sudo update-rc.d mount-usb defaults
 
@@ -6,9 +6,9 @@ sudo update-rc.d mount-usb defaults
 
 #!/bin/sh
 sudo mkdir -p /media/silic
-sudo umount /dev/sda1
+sudo umount /dev/sda1 || echo "Skipping..."
 sudo mount /dev/sda1 -t vfat -o rw,nosuid,nodev,sync,noatime,quiet,shortname=mixed,uid=1000,gid=1000,umask=077,iocharset=utf8 /media/silic
-echo 'mount to /media/silic'
+echo "mount to /media/silic"
 
 
 
@@ -17,18 +17,22 @@ cd ~/lobby/silic/; ~/.local/bin/poetry run python dev/audio2s3.py
 
 
 ### inside the crontab (RPi4):
-# @reboot cd ~/lobby/silic/; poetry run python dev/audio2s3.py
-@reboot /etc/init.d/mount-usb
-# @reboot cd ~/lobby/silic/; /home/ntupi/.local/bin/poetry run python dev/audio2s3.py >/tmp/ppylog.log &>/tmp/troubleshooting.log
-# @reboot (sleep 60 && cd ~/lobby/silic/ && /home/ntupi/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
-@reboot (sleep 5 && cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
-# */1 * * * * (sleep 5 && lsusb) >/tmp/ppylog.log 2>&1
-# */1 * * * * (cd ~/lobby/silic/; /home/ntupi/.local/bin/poetry run python test.py) >/tmp/troubleshooting.log 2>&1
+# Mounting the USB
+@reboot /etc/init.d/mount-usb >/tmp/mmount.log 2>&1
+# Run the audio script
+@reboot (sleep 5 && cd ~/lobby/silic/ && XDG_RUNTIME_DIR=/run/user/$(id -u) ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
+# For testing
+*/1 * * * * (cd ~/lobby/silic/ && XDG_RUNTIME_DIR=/run/user/$(id -u) ~/.local/bin/poetry run python dev/test_pyaudio.py) >/tmp/ppylog.log 2>&1
 
 ### inside the crontab (TX2):
-@reboot (sleep 20 && cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
-##### sudo crontab -e
+# Mounting the USB (<NOTICE !!!> Requires `sudo crontab -e`)
 @reboot (sleep 5 && /etc/init.d/mount-usb) >/tmp/mmount.log 2>&1
+
+# Run the audio script
+@reboot (sleep 15 && cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
+# For testing
+*/1 * * * * (cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/test_pyaudio.py) >/tmp/ppylog.log 2>&1
+
 # @reboot (cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
 # */1 * * * * (cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
 # */1 * * * * (cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/test_pyaudio.py) >/tmp/ppylog.log 2>&1
