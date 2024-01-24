498f1f301f66598e5a1950211bdde213f469cda1
LaZoark <emersonchen086@icloud.com>
2024-01-24 21:49:17 +0800

    crontab: sleep 60

--- mount_usb.sh
+++ mount_usb.sh
@@ -13,14 +13,14 @@ echo "mount to /media/silic"
 
 
 #!/bin/sh
-cd ~/lobby/silic/; ~/.local/bin/poetry run python dev/audio2s3.py
+cd ~/lobby/silic && ~/.local/bin/poetry run python dev/audio2s3.py
 
 
 ### inside the crontab (RPi4):
 # Mounting the USB
 @reboot /etc/init.d/mount-usb >/tmp/mmount.log 2>&1
 # Run the audio script
-@reboot (sleep 25 && cd ~/lobby/silic/ && XDG_RUNTIME_DIR=/run/user/$(id -u) ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
+@reboot (sleep 60 && cd ~/lobby/silic/ && XDG_RUNTIME_DIR=/run/user/$(id -u) ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
 # For testing
 */1 * * * * (cd ~/lobby/silic/ && XDG_RUNTIME_DIR=/run/user/$(id -u) ~/.local/bin/poetry run python dev/test_pyaudio.py) >/tmp/ppylog.log 2>&1
 
@@ -29,7 +29,7 @@ cd ~/lobby/silic/; ~/.local/bin/poetry run python dev/audio2s3.py
 @reboot (sleep 5 && /etc/init.d/mount-usb) >/tmp/mmount.log 2>&1
 
 # Run the audio script
-@reboot (sleep 25 && cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
+@reboot (sleep 60 && cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/audio2s3.py) >/tmp/ppylog.log 2>&1
 # For testing
 */1 * * * * (cd ~/lobby/silic/ && ~/.local/bin/poetry run python dev/test_pyaudio.py) >/tmp/ppylog.log 2>&1
 
